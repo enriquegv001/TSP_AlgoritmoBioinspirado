f9c728970e9d0c252e62d6ea5b6cf1bf872868d4
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2022-10-22 20:05:46 -0500

    maps

--- TSP/app.py
+++ TSP/app.py
@@ -1,21 +1,21 @@
 import streamlit as st
-import constants
+import os
 import streamlit.components.v1 as components
 
 st.title('TSP')
 st.markdown("Three different ways on getting solutions por the Travelling Salesman Problem, applying **Simplex, Ant Colony and Genetic algorithm**")
 # embed streamlit docs in a streamlit app
 st.subheader("Simplex")
-HtmlFile = open(constants.path+"maps/exact.html", 'r', encoding='utf-8')
+HtmlFile = open(os.path.join("maps/exact.html"), 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 components.html(source_code,height=400)
 
 st.subheader("Ant colony")
-HtmlFile = open("maps/ant.html", 'r', encoding='utf-8')
+HtmlFile = open(os.path.join("maps/ant.html"), 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 components.html(source_code,height=400)
 
 st.subheader("Genetic")
-HtmlFile = open("maps/genetic.html", 'r', encoding='utf-8')
+HtmlFile = open(os.path.join("maps/genetic.html"), 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 components.html(source_code,height=400)
