e174920a1c82bbdb126fc9a85546b4ab51ab74b0
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2022-10-22 19:43:35 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title('TSP')
+st.markdown("Three different ways on getting solutions por the Travelling Salesman Problem, applying **Simplex, Ant Colony and Genetic algorithm**")
+# embed streamlit docs in a streamlit app
+st.subheader("Simplex")
+HtmlFile = open("maps/exact.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read()
+components.html(source_code,height=400)
+
+st.subheader("Ant colony")
+HtmlFile = open("maps/ant.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read()
+components.html(source_code,height=400)
+
+st.subheader("Genetic")
+HtmlFile = open("maps/genetic.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read()
+components.html(source_code,height=400)
